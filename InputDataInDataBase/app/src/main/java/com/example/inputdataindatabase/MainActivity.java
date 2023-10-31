2c6e882c8da2d5bef45d8763291a2336833f837a
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-31 14:41:17 +0900

    고객관리 데이터베이스 액티비티 세분화(메인)

--- InputDataInDataBase/app/src/main/java/com/example/inputdataindatabase/MainActivity.java
+++ InputDataInDataBase/app/src/main/java/com/example/inputdataindatabase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.inputdataindatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
@@ -15,7 +16,7 @@ import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DRIVER = "oracle.jdbc.OracleDriver";
-    private static final String URL = "jdbc:oracle:thin:@myIPv4Address:port:xe";
+    private static final String URL = "jdbc:oracle:thin:@IPv4Address:port:xe";
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
     private Connection connection;
@@ -28,66 +29,50 @@ public class MainActivity extends AppCompatActivity {
         // 보안정책 관련
         StrictMode.ThreadPolicy threadpolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(threadpolicy);
-    }
 
-    public void onClickedBtn(View view){
-        TextView cusID = findViewById(R.id.cusID);
-        TextView cusName = findViewById(R.id.cusName);
-        TextView cusPhone = findViewById(R.id.phone);
-        TextView cusBirth = findViewById(R.id.birth);
-        TextView result = findViewById(R.id.result);
-        Spinner subject = findViewById(R.id.subjects);
+        TextView totalCnt = findViewById(R.id.totalCnt);
+        TextView newTotalCnt = findViewById(R.id.newCnt);
 
-        String id = cusID.getText().toString();
-        String name = cusName.getText().toString();
-        String phone = cusPhone.getText().toString();
-        String birth = cusBirth.getText().toString();
-        String query = "";
-
-        switch (subject.getSelectedItem().toString()){
-            case "입력":
-                query = "INSERT INTO MART_CUS(CUS_ID, CUS_NAME, TEL, BIRTH) VALUES('"+id+"','"+name+
-                        "','"+phone+"','"+birth+"')";
-                break;
-            case "전체 조회":
-                query = "SELECT * FROM MART_CUS ORDER BY CUS_ID";
-                break;
-            case "삭제":
-                query = "DELETE FROM MART_CUS WHERE CUS_ID="+id;
-                break;
-            case "수정":
-                id = cusID.getText().toString();
-                name = cusName.getText().toString();
-                phone = cusPhone.getText().toString();
-                birth = cusBirth.getText().toString();
-
-                query = "UPDATE MART_CUS SET CUS_NAME='"+name+"',TEL='"+phone+"',BIRTH='"+birth+"'"+" WHERE CUS_ID="+id;
-                break;
-        }
+        Intent fromInputNewData = getIntent();
+        int newCnt = fromInputNewData.getIntExtra("newCnt", 0);
+        newTotalCnt.setText(String.valueOf(newCnt));
 
         try {
             Class.forName(DRIVER); // DRIVER
             this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
             Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM MART_CUS");
             StringBuffer stringBuffer = new StringBuffer();
-            ResultSet resultSet = statement.executeQuery(query);
 
-            if (! subject.getSelectedItem().toString().equals("전체 조회"))
-                result.setText(subject.getSelectedItem().toString() + " 완료 !");
-            else {
-                while (resultSet.next()) {
-                    stringBuffer.append(resultSet.getString(1) + "  ");
-                    stringBuffer.append(resultSet.getString(2) + "  ");
-                    stringBuffer.append(resultSet.getString(3) + "  ");
-                    stringBuffer.append(resultSet.getString(4) + "  ");
-                    stringBuffer.append("\n");
-                }
-                result.setText(String.format("%-10s", stringBuffer.toString()));
+            while(resultSet.next()){
+                stringBuffer.append(resultSet.getString(1) + " ");
+                stringBuffer.append(resultSet.getString(2) + " ");
+                stringBuffer.append(resultSet.getString(3) + " ");
+                stringBuffer.append(resultSet.getString(4) + " ");
+                stringBuffer.append("\n");
             }
 
+            totalCnt.setText(String.valueOf(stringBuffer.toString().split("\n").length));
         }catch (Exception e){
-            result.setText(e.toString());
+            totalCnt.setText(e.toString());
         }
+    }
+
+    public void onClickedGetAllData(View view){
+        Intent getAll = new Intent(this, GetAllData.class);
+        startActivity(getAll);
+    }
+    public void onClickedInputNewData(View view){
+        Intent inputNewData = new Intent(this, InputNewData.class);
+        startActivity(inputNewData);
+    }
+    public void onClickedDeleteData(View view){
+        Intent deleteData = new Intent(this, DeleteData.class);
+        startActivity(deleteData);
+    }
+    public void onClickedModifyData(View view){
+        Intent modifyData = new Intent(this, ModifyData.class);
+        startActivity(modifyData);
     }
 }
