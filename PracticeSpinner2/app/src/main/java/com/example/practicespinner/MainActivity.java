ec252e15a4062a01ad89a7bd2edc06cccd369aa3
LeeHeonWoo1 <gjsdn245@naver.com>
2023-11-07 22:01:02 +0900

    delete before project

--- PracticeSpinner2/app/src/main/java/com/example/practicespinner/MainActivity.java
+++ PracticeSpinner2/app/src/main/java/com/example/practicespinner/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -11,11 +12,16 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    String[] items = {"일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spin2 = findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        spin2.setAdapter(adapter);
     }
 
     public void onClicked(View view){
