f364746fb7d2ef0fec3c05ce0606c33e37ce824b
LeeHeonWoo1 <gjsdn245@naver.com>
2023-11-11 15:49:43 +0900

    class for package item

---
+++ TATProject/app/src/main/java/com/example/tatproject/ForRecyclerView/PackageSingleItem.java
@@ -0,0 +1,13 @@
+package com.example.tatproject.ForRecyclerView;
+
+public class PackageSingleItem {
+    String packageID;
+    public String title;
+    String price;
+
+    public PackageSingleItem(String title, String packID, String price){
+        this.packageID = packID;
+        this.title = title;
+        this.price = price;
+    }
+}
