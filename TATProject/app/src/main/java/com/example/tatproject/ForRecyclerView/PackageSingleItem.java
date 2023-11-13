1bb3697bd19314cd45203f8b3fec988df66e6c51
LeeHeonWoo1 <gjsdn245@naver.com>
2023-11-13 22:37:10 +0900

    package list recycler view item

--- TATProject/app/src/main/java/com/example/tatproject/ForRecyclerView/PackageSingleItem.java
+++ TATProject/app/src/main/java/com/example/tatproject/ForRecyclerView/PackageSingleItem.java
@@ -4,12 +4,14 @@ public class PackageSingleItem {
     String packageID;
     public String title;
     String price;
-    int articleNum;
+    int resID;
+    int resID2;
 
-    public PackageSingleItem(String title, String packID, String price, int num){
+    public PackageSingleItem(String title, String packID, String price, int resID, int resID2){
         this.packageID = packID;
         this.title = title;
         this.price = price;
-        this.articleNum = num;
+        this.resID = resID;
+        this.resID2 = resID2;
     }
 }
