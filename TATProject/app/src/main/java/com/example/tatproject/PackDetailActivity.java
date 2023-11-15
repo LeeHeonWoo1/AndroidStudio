77e7874cbcd10eca50073ec588805158d04d2789
LeeHeonWoo1 <gjsdn245@naver.com>
2023-11-15 09:13:19 +0900

    package detail activitY

--- TATProject/app/src/main/java/com/example/tatproject/PackDetailActivity.java
+++ TATProject/app/src/main/java/com/example/tatproject/PackDetailActivity.java
@@ -12,6 +12,7 @@ import android.os.StrictMode;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,12 +29,15 @@ public class PackDetailActivity extends AppCompatActivity {
     // 본인 DB계정으로 변경
     private static final String DRIVER = "oracle.jdbc.OracleDriver";
 
-    private static final String URL = "jdbc:oracle:thin:@192.168.30.4:1521:xe";
-    private static final String USERNAME = "c##group";
-    private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:oracle:thin:@IPv4Address:port:xe";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
     private Connection connection;
 
-    private String username;
+    private String username = ""; // 유저 이름
+    private String userid = ""; // 유저 id
+    private String id = ""; // 상품 넘버
+    private String bookID = ""; // 예약 넘버
 
     ImageView imageView;
     Bitmap bitmap;
@@ -44,18 +48,23 @@ public class PackDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pack_detail);
 
-        Intent fromMain = getIntent();
-        this.username = fromMain.getStringExtra("username");
+        Intent fromPackList = getIntent();
+        this.username = fromPackList.getStringExtra("username");
+        this.userid = fromPackList.getStringExtra("user");
+        this.bookID = fromPackList.getStringExtra("id");
 
         TextView logedout = findViewById(R.id.logedout);
         TextView login = findViewById(R.id.logined);
+        TextView logout = findViewById(R.id.logout4);
 
         if (username.equals("")){
             logedout.setVisibility(View.VISIBLE);
             login.setVisibility(View.INVISIBLE);
+            logout.setVisibility(View.INVISIBLE);
         }else {
             logedout.setVisibility(View.INVISIBLE);
             login.setVisibility(View.VISIBLE);
+            logout.setVisibility(View.VISIBLE);
         }
 
         //보안정책
@@ -85,11 +94,10 @@ public class PackDetailActivity extends AppCompatActivity {
 
             Intent getIntent = getIntent();
             String title = getIntent.getStringExtra("title");
-            String id = getIntent.getStringExtra("id");
 
             ResultSet resultSet = statement.executeQuery
                     ("SELECT PACK_NAME, to_char(JOURNEY_START,'yyyy-mm-dd'),CONTENTS,PRICE,IMG_URL" +
-                            ",to_char(JOURNEY_END,'yyyy-mm-dd') FROM CREATE_PACK WHERE PACK_ID = "+id);
+                            ",to_char(JOURNEY_END,'yyyy-mm-dd') FROM CREATE_PACK WHERE PACK_ID = "+bookID);
 
             while(resultSet.next()){
                 titleBuffer.append(resultSet.getString(1));
@@ -150,5 +158,79 @@ public class PackDetailActivity extends AppCompatActivity {
         }catch (InterruptedException e){
             e.printStackTrace();
         }
+    }
+
+    public void onClickedBook(View view){
+        Intent intent = new Intent(PackDetailActivity.this, BookActivity.class);
+
+        try{
+            TextView tv = findViewById(R.id.textView8);
+
+            Class.forName(DRIVER);
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+            StringBuffer bookBuffer = new StringBuffer();
+            StringBuffer mileBuffer = new StringBuffer();
+            StringBuffer bookIDBuffer = new StringBuffer();
+
+            //마일리지 적립, 기존에 있는 예약 ID 불러옴
+            ResultSet resultSet = statement.executeQuery("SELECT MILE, BOOK_ID FROM CUS_USER WHERE USERID = '"+userid+"'");
+            while (resultSet.next()) {
+                mileBuffer.append(resultSet.getString(1));
+                bookIDBuffer.append(resultSet.getString(2));
+            }
+            String getBookID = bookIDBuffer.toString();
+
+            String mile = String.valueOf (Integer.parseInt(mileBuffer.toString()) + 100) ;
+            String[] gradeArr = {"bronze","silver","gold","platinum","diamond"};
+            int arrNum = Integer.parseInt(mile)/500;
+            //tv.setText(gradeArr[arrNum]);
+
+            ResultSet resultSet2 = statement.executeQuery
+                    ("SELECT PACK_ID FROM CREATE_PACK WHERE PACK_ID = "+bookID); // 상품 id 추출
+
+            while(resultSet2.next()){
+                bookBuffer.append(resultSet2.getString(1));
+            }
+            String book_id = bookBuffer.toString();
+
+            if(getBookID.contains("null")){
+                // 예약 update
+                ResultSet resultSet3 = statement.executeQuery(
+                        "UPDATE CUS_USER SET BOOK_ID =" + book_id +", " +
+                                "BOOK_DATE = TO_DATE(TO_CHAR(SYSDATE, 'YYYYMMDD'), 'YYYYMMDD'), MILE = "+mile+
+                                ",RANK = '"+gradeArr[arrNum]+"' WHERE USERID = '" + userid + "'");
+                //Toast.makeText(this, book_id, Toast.LENGTH_LONG).show();
+
+                intent.putExtra("user",userid);
+                // intent.putExtra("book_id",book_id);
+                startActivity(intent);
+
+            }else{
+                Toast.makeText(this, "이미 처리된 예약입니다.", Toast.LENGTH_LONG).show();
+            }
+        }catch (Exception e){
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickedToMypage(View view){
+        Intent toMypage = new Intent(this, MyPageActivity.class);
+        toMypage.putExtra("user", userid);
+        toMypage.putExtra("username", username);
+
+        startActivity(toMypage);
+    }
+
+    public void onClickedLogout(View view){
+        Intent toLogin = new Intent(this, LoginActivity.class);
+        startActivity(toLogin);
+    }
+
+    public void onClickedToMain(View view){
+        Intent toMain = new Intent(this, MainActivity.class);
+        toMain.putExtra("username", username);
+        toMain.putExtra("user", userid);
+        startActivity(toMain);
     }
 }
