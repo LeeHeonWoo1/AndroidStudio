ff90c3c8f84041f61b5c7347e40ae7a8031b782e
LeeHeonWoo1 <gjsdn245@naver.com>
2023-11-15 09:06:54 +0900

    modify for image to article number

--- TATProject/app/src/main/java/com/example/tatproject/ForRecyclerView/SingleItem.java
+++ TATProject/app/src/main/java/com/example/tatproject/ForRecyclerView/SingleItem.java
@@ -6,12 +6,12 @@ public class SingleItem {
     public String QATitle;
     public String writer;
     public String writeDate;
-    public int resID;
+    public int articleNum;
 
-    public SingleItem(String title, String writer, String writeDate, int resID){
+    public SingleItem(String title, String writer, String writeDate, int num){
         this.QATitle = title;
         this.writer = writer;
         this.writeDate = writeDate;
-        this.resID = resID;
+        this.articleNum = num;
     }
 }
