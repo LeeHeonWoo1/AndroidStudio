de57f0881112d5cd1312a86f24bf168afd3922e7
LeeHeonWoo1 <gjsdn245@naver.com>
2023-11-15 09:10:57 +0900

    connect other activity from main activity

--- TATProject/app/src/main/java/com/example/tatproject/MainActivity.java
+++ TATProject/app/src/main/java/com/example/tatproject/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private int num_page = 4;
     private CircleIndicator3 mIndicator;
     private String username = "";
+    private String userid = "";
     private static int renderCnt = 0;
 
     public void setUsername(String username) {
@@ -33,18 +34,23 @@ public class MainActivity extends AppCompatActivity {
 
         if (! (renderCnt == 0)){
             Intent fromLogin = getIntent();
+
+            this.userid = fromLogin.getStringExtra("user");
             this.username = fromLogin.getStringExtra("username");
         }
 
         TextView logedout = findViewById(R.id.logedout);
         TextView login = findViewById(R.id.logined);
+        TextView logout = findViewById(R.id.logout);
 
         if (username.equals("")){
             logedout.setVisibility(View.VISIBLE);
             login.setVisibility(View.INVISIBLE);
+            logout.setVisibility(View.INVISIBLE);
         }else {
             logedout.setVisibility(View.INVISIBLE);
             login.setVisibility(View.VISIBLE);
+            logout.setVisibility(View.VISIBLE);
         }
 
 //      ViewPager2
@@ -93,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Intent toComplain = new Intent(this, ComplainOrQA.class);
             toComplain.putExtra("username", this.username);
+            toComplain.putExtra("user", this.userid);
 
             startActivity(toComplain);
         }
@@ -102,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         Intent toPackList = new Intent(this, PackageActivity.class);
 
         toPackList.putExtra("username", this.username);
+        toPackList.putExtra("user", this.userid);
         startActivity(toPackList);
     }
 
@@ -113,6 +121,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickedToMyPage(View view){
+        Intent toMypage = new Intent(this, MyPageActivity.class);
 
+        toMypage.putExtra("user", this.userid);
+        toMypage.putExtra("username", username);
+        startActivity(toMypage);
+    }
+
+    public void onClickedToBookmark(View view){
+        if (username.equals("")){
+            Snackbar.make(view, "로그인을 먼저 진행해주세요.", Snackbar.LENGTH_LONG).show();
+        }else{
+            Intent toBookmark = new Intent(this, BookMarkActivity.class);
+            toBookmark.putExtra("username", username);
+            toBookmark.putExtra("user", userid);
+
+            startActivity(toBookmark);
+        }
+    }
+
+    public void onClickedToOneByOne(View view){
+        Intent toOne = new Intent(this, OneByOne.class);
+        toOne.putExtra("username", username);
+        toOne.putExtra("user", userid);
+
+        startActivity(toOne);
+    }
+
+    public void onClickedLogout(View view){
+        Intent toLogin = new Intent(this, LoginActivity.class);
+        startActivity(toLogin);
     }
 }
