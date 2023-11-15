2ef1d86aa32f8d6171ab210d2a98a2e45cb95e82
LeeHeonWoo1 <gjsdn245@naver.com>
2023-11-15 09:09:32 +0900

    make progress bar of answer percentage

--- TATProject/app/src/main/java/com/example/tatproject/ComplainOrQA.java
+++ TATProject/app/src/main/java/com/example/tatproject/ComplainOrQA.java
@@ -2,6 +2,10 @@ package com.example.tatproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,12 +19,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+
 public class ComplainOrQA extends AppCompatActivity {
     private static final String DRIVER = "oracle.jdbc.OracleDriver";
-
-    private static final String URL = "jdbc:oracle:thin:@192.168.30.4:1521:xe";
-    private static final String USERNAME = "c##group";
-    private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:oracle:thin:@IPv4Address:port:xe";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
     private Connection connection;
     private String username;
     private String writer;
@@ -28,6 +32,7 @@ public class ComplainOrQA extends AppCompatActivity {
     private String writeDate;
     private RecyclerViewAdapter adapter;
     ArrayList<SingleItem> list = new ArrayList<>();
+    private String userid = "";
 
     @Override
     public void onCreate(Bundle savedInstance){
@@ -36,27 +41,55 @@ public class ComplainOrQA extends AppCompatActivity {
 
         Intent fromMain = getIntent();
         this.username = fromMain.getStringExtra("username");
+        this.userid = fromMain.getStringExtra("user");
+
+        ProgressBar pr = findViewById(R.id.progress_bar);
+        TextView quetisonCntText = findViewById(R.id.questionCnt);
+        TextView answerPercent = findViewById(R.id.answerPercent);
 
         try{
             Class.forName(DRIVER);
             this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT TITLE, WRITE_DATE, USER_WRITER FROM QA");
+            ResultSet resultSet = statement.executeQuery("SELECT TITLE, WRITE_DATE, USER_WRITER, ANSWER_CONTENT FROM QA");
             StringBuffer stringBuffer = new StringBuffer();
+            StringBuffer answered = new StringBuffer();
 
             while(resultSet.next()){
                 stringBuffer.append(resultSet.getString(1 )+ ",");
                 stringBuffer.append(resultSet.getString(2 )+ ",");
                 stringBuffer.append(resultSet.getString(3 )+ ",");
+                answered.append(resultSet.getString(4) + ",");
             }
+
             int idx = 0;
             String[] stringArr = stringBuffer.toString().split(",");
+            int questionCnt = stringArr.length / 3; // 질문개수
+            String[] answerArr = answered.toString().split(",");
+            float answerCnt = 0;
+            for(String i : answerArr){
+                if (! i.equals("null")){ // null이 아니다 = 답변이 있다
+                    answerCnt += 1.0;
+                }
+            }
 
+            int num = 1;
             for (int i = 0; i < stringArr.length / 3; i++) {
-                list.add(new SingleItem(stringArr[idx], stringArr[idx + 1], stringArr[idx + 2], R.drawable.lock_icon));
+                list.add(new SingleItem(stringArr[idx], stringArr[idx + 1], stringArr[idx + 2], num++));
                 idx += 3;
             }
+
+            quetisonCntText.setText(String.valueOf(questionCnt));
+            String condition = "";
+            if ((answerCnt / questionCnt) * 100.0f >= 100){
+                condition = String.valueOf((answerCnt / (float)questionCnt) * 100.0f).substring(0, 3);
+            }else{
+                condition = String.valueOf((answerCnt / (float)questionCnt) * 100.0f).substring(0, 2);
+            }
+
+            pr.setProgress(Integer.parseInt(condition));
+            answerPercent.setText(Integer.parseInt(condition) + "%");
 //          recyclerView, adapter연결
             RecyclerView recyclerView = findViewById(R.id.recyclerView);
             recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
@@ -68,6 +101,41 @@ public class ComplainOrQA extends AppCompatActivity {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
 
+    public void onClickedToWrite(View view){
+        Intent toWrite = new Intent(this, WriteQA.class);
+        toWrite.putExtra("username", username);
+
+        startActivity(toWrite);
+    }
+
+    public void onClickedToBookMark(View view){
+        Intent toBookMark = new Intent(this, BookMarkActivity.class);
+        toBookMark.putExtra("username", username);
+        toBookMark.putExtra("user", this.userid);
+
+        startActivity(toBookMark);
+    }
+
+    public void onClickedToPackList(View view){
+        Intent packList = new Intent(this, PackageActivity.class);
+        packList.putExtra("username", username);
+        packList.putExtra("user", this.userid);
+
+        startActivity(packList);
+    }
+
+    public void onClickedToOneByOne(View view){
+        Intent toOneByOne = new Intent(this, OneByOne.class);
+        toOneByOne.putExtra("username", username);
+        toOneByOne.putExtra("user", this.userid);
+
+        startActivity(toOneByOne);
+    }
+
+    public void onClickedLogout(View view){
+        Intent toLogin = new Intent(this, LoginActivity.class);
+        startActivity(toLogin);
     }
 }
